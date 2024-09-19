b1c5435e96919dc00435d0df4b077ae3496a71c9
Max <tongweizj@gmail.com>
2024-09-18 22:27:14 -0400

    update

---
+++ comp123_programming/week02/PetDemo/Pet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetDemo
+{
+    internal class Pet
+    {
+        // data
+
+        public string Name { get; set; }
+        public  string Owner { get; private set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+        public bool IsHouseTrained { get; private set; }
+
+        // constractor
+        public Pet(string name, int age, string description)
+        {
+            Name = name;
+            Age = age;
+            Description = description;
+            Owner = "no one";
+            IsHouseTrained = false;
+        }
+        public override string ToString() { 
+            return $"Pet's name: {Name}, age: {Age}, it is a {Description}, it's owner is {Owner}, it has traned {IsHouseTrained} ";
+        }
+
+        // method
+
+        public void Train()
+        {
+
+        }
+
+        public void SetOwner(string owner) 
+        { 
+        }
+    }
+}

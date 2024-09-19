9bc90d2a408b8074c28d8c1f684e644c223b9a1a
Max <tongweizj@gmail.com>
2024-09-19 00:05:52 -0400

    update

--- comp123_programming/week02/lab_medal/Properties/AssemblyInfo.cs
+++ comp123_programming/week02/lab_medal/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pet")]
+[assembly: AssemblyTitle("lab_medal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Centennial College")]
-[assembly: AssemblyProduct("Pet")]
-[assembly: AssemblyCopyright("Copyright © Centennial College 2024")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab_medal")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4869424a-e06a-418c-94b6-8a94be529f29")]
+[assembly: Guid("b832b606-1404-4181-bc2d-aac22de7b439")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

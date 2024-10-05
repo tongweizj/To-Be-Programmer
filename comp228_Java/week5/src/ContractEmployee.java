6089495f82ef1ee866be2805eaeef944f95ffb5b
tongweizj <tongweizj@gmail.com>
2024-10-05 10:57:24 -0400

    update

---
+++ comp228_Java/week5/src/ContractEmployee.java
@@ -0,0 +1,17 @@
+/*
+ContractEmployee inherits from Employee class
+Employess class is inherited in to ContractEmployee
+
+Employee - parent or super or base class
+contractEmployee - child or sub or derived
+
+all the properties and method from the Employee class
+can be used in ContractEmployee class
+*/
+
+public class CountractEmployee extends Employee {
+    private double contractAmount;
+    private int duration;
+
+//    public void
+}

6ac3f37cf78fea32150427999978a266884270a7
Max <tongweizj@gmail.com>
2024-11-20 19:34:00 -0400

    20241120 cSharp update week8 lab

---
+++ C-sharp_c123/Week08/BankingApplication/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApplication
+{
+    internal abstract class Account
+    {
+        public readonly List<Person> Holders = new List<Person>();
+        public readonly List<Transaction> Transactions = new List<Transaction>();
+        public readonly String Number;
+        private int LAST_NUMBER;
+
+        public Double Balance { get; protected set; }
+        public Double LowestBalance { get; protected set; }
+
+        public Account(string type, double balance)
+        {
+            Balance = balance;
+            LowestBalance = balance;
+            Number = type + LAST_NUMBER;
+            LAST_NUMBER++;
+        }
+    }
+}

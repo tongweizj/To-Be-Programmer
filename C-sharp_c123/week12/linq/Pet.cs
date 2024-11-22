891baf83bf4898e0bdfc04fd726de77bda378ecc
tongweizj <tongweizj@gmail.com>
2024-11-21 22:04:43 -0500

    20241121c#

---
+++ C-sharp_c123/week12/linq/Pet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Linq
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+        public PetOwner Owner { get; set; }
+    }
+
+}
+
---
+++ C-sharp_c123/week12/linq/PetOwner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Linq
+{
+    public class PetOwner
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+}
+

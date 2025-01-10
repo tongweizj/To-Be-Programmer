ebfd1073bd312724120386329b48660225754d9d
Max <tongweizj@gmail.com>
2025-01-09 22:55:09 -0500

    20250109

--- Java_c304/HelloWorld/app/src/main/java/com/centennial/helloworld/MainActivity.kt
+++ Java_c304/HelloWorld/app/src/main/java/com/centennial/helloworld/MainActivity.kt
@@ -1,11 +1,9 @@
-package com.example.demo
+package com.centennial.helloworld
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,17 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.demo.ui.theme.DemoTheme
+import com.centennial.helloworld.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DemoTheme {
+            HelloWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Max",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -34,33 +32,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-
-        Text(
-            text = "Hello again!",
-            modifier = modifier
-        )
-
-        Row {
-            Text(
-                text = "Hello Row!",
-                modifier = modifier
-            )
-        }
-    }
-
-
-
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DemoTheme {
+    HelloWorldTheme {
         Greeting("Android")
     }
 }
